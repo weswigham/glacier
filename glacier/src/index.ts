ef2ee96e4ae2e3b0cbf3a6d60b4a3a2580fbefcc
Wesley Wigham <wwigham@gmail.com>
2016-10-06 12:47:48 -0400

    Redux

--- glacier/src/index.ts
+++ glacier/src/index.ts
@@ -1,2 +1,39 @@
+import * as redux from "redux";
 
-export {}
+export interface ModelState {}
+export type Message = {}
+export type SourceType = "sqlite";
+
+export function createModel() {
+    return redux.createStore((state: ModelState, action: Message) => {
+        // TODO: Define actions and handle state changes
+        return state;
+    });
+}
+
+export function createSqliteDataSource() {
+    return createDataSource("sqlite");
+}
+
+export function createDataSource(type: SourceType) {
+    return new class {
+
+    };
+}
+
+export interface Exporter {
+    (): void; // OnUpdate method
+    export(): string;
+}
+
+
+export function createExporter(store: redux.Store<ModelState>) {
+    const updater: Exporter = ((() => {
+        // On update...
+        store.getState()
+    }) as Exporter)
+    updater.export = () => {
+        return "";
+    }
+    return updater;
+}
