143b51d65f196fdfc8fd21ef31843973f7e1cf72
Zach Calfin <zpc4747@g.rit.edu>
2016-11-16 00:37:49 +0000

    more testing changes, still broken

--- glacier/src/test/importer.spec.ts
+++ glacier/src/test/importer.spec.ts
@@ -24,62 +24,61 @@ describe("glacier as a model", () => {
         const adapter = glacier.createSqlFileDataSource(model, "../data/CycleChain.sqlite");
         const exporter = glacier.createSvgExporter(model);
         // TODO: Update this example to appropriately insert encodings once encodings are held within the store
-        // adapter.updateCache().then(() => exporter.export().then(value => {
-        //     expect(value).to.be.a("string");
-        // //    expect(value).to.be.equal(require("fs").readFileSync("../data/visualization.svg").toString());
-        //     const xml = require("fs").readFileSync("../data/visualization.svg").toString();
-        //     let tOne = new Array();
-        //     let tTwo = new Array();
-        //     let firstEles = new Array();
-        //     let secondEles = new Array();
-        //     const recurse = function(xmljs:any){
-        //         let eles = 0;
-        //         let gElements = 0;
-        //         if ( xmljs instanceof Object) {
-        //             const keys = Object.keys(xmljs);
-        //             eles += keys.length;
-        //             for (var i = 0; i < keys.length; i++) {
-        //                 let key = keys[i];
-        //                 if (key == "g"){
-        //                     ++gElements;
-        //                 }
-        //                 if(key=="DaysToManufacture"){
-        //                     console.log(xmljs[key]);
-        //                 }
-        //                 let resp = recurse(xmljs[key]);
-        //                 eles += resp[0];
-        //                 gElements += resp[1];
+        adapter.updateCache().then(() => exporter.export().then(value => {
+            expect(value).to.be.a("string");
+        //    expect(value).to.be.equal(require("fs").readFileSync("../data/visualization.svg").toString());
+            const xml = require("fs").readFileSync("../data/visualization.svg").toString();
+            let tOne = new Array();
+            let tTwo = new Array();
+            let firstEles = new Array();
+            let secondEles = new Array();
+            const recurse = function(xmljs:any){
+                let eles = 0;
+                let gElements = 0;
+                if ( xmljs instanceof Object) {
+                    const keys = Object.keys(xmljs);
+                    eles += keys.length;
+                    for (var i = 0; i < keys.length; i++) {
+                        let key = keys[i];
+                        if (key == "g"){
+                            ++gElements;
+                        }
+                        if(key=="DaysToManufacture"){
+                            console.log(xmljs[key]);
+                        }
+                        let resp = recurse(xmljs[key]);
+                        eles += resp[0];
+                        gElements += resp[1];
                         
-        //             }
-        //         }
-        //         if ( xmljs instanceof Array ) {
-        //             for (var i = 0; i < xmljs.length; i++) {
-        //               // console.log(xmljs);
-        //                 let resp = recurse(xmljs[i]);
-        //                 eles += resp[0];
-        //                 gElements += resp[1];
-        //             }
-        //         }
+                    }
+                }
+                if ( xmljs instanceof Array ) {
+                    for (var i = 0; i < xmljs.length; i++) {
+                      // console.log(xmljs);
+                        let resp = recurse(xmljs[i]);
+                        eles += resp[0];
+                        gElements += resp[1];
+                    }
+                }
                 
-        //         let response = [eles, gElements];
-        //         return response;
-        //     }
+                let response = [eles, gElements];
+                return response;
+            }
             
-        //     parseString(xml, function(err, result){
-        //         tOne = recurse(result);
-        //         firstEles = Object.keys(result.svg);
-        //     });
-        //     parseString(value, function(err, result){
-        //         tTwo = recurse(result);
-        //         secondEles = Object.keys(result.svg);
-        //     });
+            parseString(xml, function(err, result){
+                tOne = recurse(result);
+                firstEles = Object.keys(result.svg);
+            });
+            parseString(value, function(err, result){
+                tTwo = recurse(result);
+                secondEles = Object.keys(result.svg);
+            });
             
-        //     expect(tOne[0]).to.be.equal(tTwo[0]);
-        //     expect(tOne[1]).to.be.equal(tTwo[1]);
-        //     //expect(firstEles).to.be.equal(secondEles);
-        //     adapter.remove();
-        //     done();
-        // }).catch(err => done(err)));
-        console.log(adapter.updateCache());
+            expect(tOne[0]).to.be.equal(tTwo[0]);
+            expect(tOne[1]).to.be.equal(tTwo[1]);
+            //expect(firstEles).to.be.equal(secondEles);
+            adapter.remove();
+            done();
+        }).catch(err => done(err)));
     });
 });
