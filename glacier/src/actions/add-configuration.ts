d63c7e8b575f9e76aa38dea8b554de161eec88fe
Steven <sjg8961@rit.edu>
2016-11-17 12:20:21 -0500

    Revert "First pass at more actions"
    This reverts commit 09c4da9996c7696371186a17c74ac1c28185fcda.
    
    # Conflicts:
    #	glacier/src/actions/index.ts
    #	glacier/src/model/index.ts

--- glacier/src/actions/add-configuration.ts
+++
@@ -1,9 +0,0 @@
-import {ReduxStandardAction} from "./";
-
-let uuid = 0;
-
-export type AddConfigurationAction = ReduxStandardAction<"ADD_CONFIGURATION", {uuid: number}>;
-
-export function createAddConfiguration(uuid : number): AddConfigurationAction {
-    return { type: "ADD_CONFIGURATION", payload: {uuid:uuid++}};
-}
