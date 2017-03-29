861cf379b1dcde8762fc9578e0df3fbb3b51a4d5
Wesley Wigham <wwigham@gmail.com>
2017-03-29 16:20:11 -0400

    Add size check to baseliner

--- glacier/src/test/importer.spec.ts
+++ glacier/src/test/importer.spec.ts
@@ -65,6 +65,14 @@ function baseline(name: string, actualString: string): Promise<{ expected: Docum
                     expect(evaluate("//g", actual, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE).snapshotLength).to.equal(evaluate("//g", expected, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE).snapshotLength);
                     expect(evaluate("//*", actual, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE).snapshotLength).to.equal(evaluate("//*", expected, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE).snapshotLength);
 
+                    const delta = 5; // Due to platform differences, sometimes generated sizes are (slightly) different between platforms
+                    const ewidth = +(expected.documentElement.getAttribute("width") || 0);
+                    const eheight = +(expected.documentElement.getAttribute("height") || 0);
+
+                    const awidth = +(actual.documentElement.getAttribute("width") || 0);
+                    const aheight = +(actual.documentElement.getAttribute("height") || 0);
+                    expect(awidth).to.be.closeTo(ewidth, delta);
+                    expect(aheight).to.be.closeTo(eheight, delta);
                     return resolve({ expected, actual });
                 }
                 catch (e) {
