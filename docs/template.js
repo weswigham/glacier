1b294c00de3e383193f09c78b0101739cc5cfcdc
Wesley Wigham <wwigham@gmail.com>
2017-04-04 01:21:04 -0400

    Sort test result page by number in test svg name

--- docs/template.js
+++ docs/template.js
@@ -2,9 +2,18 @@ const handlebars = require("handlebars");
 const fileContents = require("fs").readFileSync("docs/index.html.handelbars", "utf8");
 const result = handlebars.compile(fileContents);
 const date = new Date();
-const localFiles = require("fs").readdirSync("docs/baselines/local").filter(f => f.endsWith(".svg"));
-localFiles.shift();
-localFiles.sort();
+const localFiles = require("fs").readdirSync("docs/baselines/local").filter(f => f.endsWith(".svg")).sort((f1, f2) => {
+    const filename1 = f1.substring(f1.lastIndexOf("/"));
+    const filename2 = f2.substring(f2.lastIndexOf("/"));
+    const [f1p1, f1p2] = filename1.split("-");
+    const [f2p1, f2p2] = filename2.split("-");
+    if (f1p2 && f2p2) {
+        return +f1p1 - +f2p1; // Numbers seperated by a dash are sorted numerically and placed first
+    }
+    else {
+        return filename1.localeCompare(filename2);
+    }
+});
 const data = {
     "date" : date,
     "files" : localFiles,
