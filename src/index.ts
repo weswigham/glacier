fc1f1ed5cc84c52d8d678e2103f47bf83fcd9382
Wesley Wigham <wwigham@gmail.com>
2017-05-07 19:44:44 -0400

    Export combined reducers for convinience

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ export * from "./actions";
 export * from "./mapper";
 
 import {createStore, combineReducers, Store} from "redux";
-import * as reducers from "./reducers";
+import * as allReducers from "./reducers";
 import {ModelState} from "./model";
+import {AllActions} from "./actions";
 
+
+export const reducer: (state: ModelState, action: AllActions) => ModelState = combineReducers<ModelState>(allReducers as any as {[index: string]: () => any});
 export function createModel(): Store<ModelState> {
-    return createStore(combineReducers<ModelState>(reducers as any as {[index: string]: () => any}));
+    return createStore(reducer);
 }
 
 export * from "./adapters";
