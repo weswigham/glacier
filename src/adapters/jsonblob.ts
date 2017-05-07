eadf734d05b3f0926fbd46f41d647f8c5dc77568
Wesley Wigham <wwigham@gmail.com>
2017-05-07 19:36:02 -0400

    Make adapters amenable to running under a substate

--- src/adapters/jsonblob.ts
+++ src/adapters/jsonblob.ts
@@ -1,12 +1,11 @@
 import redux = require("redux");
-import { ModelState } from "../";
 import { DataAdapter } from "./";
 import { createAddDataSourceAction } from "../actions";
 import { SinglyLoadedMemoryDataSource } from "./single-memory-load-base";
 
 export interface JSONDataSourceAdapter extends DataAdapter {}
 
-export function createJSONDataSource(store: redux.Store<ModelState>, content: string): JSONDataSourceAdapter {
+export function createJSONDataSource(store: redux.Store<{}>, content: string): JSONDataSourceAdapter {
     const storedData = JSON.parse(content);
     const adapter: JSONDataSourceAdapter = new SinglyLoadedMemoryDataSource(storedData, store);
     const createAction = createAddDataSourceAction("jsonblob", {}, {}, adapter);
